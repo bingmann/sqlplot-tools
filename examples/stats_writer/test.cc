3e84cfc355baa848861ce70b0ceae6ed38663be2
Timo Bingmann <tbgit@panthema.net>
2014-03-15 12:05:39 +0100

    Adding stats_writer with example for more complex experiments.

---
+++ examples/stats_writer/test.cc
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * examples/stats_writer/test.cc
+ *
+ * Example for stats_writer
+ *
+ *******************************************************************************
+ * Copyright (C) 2014 Timo Bingmann <tb@panthema.net>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+#include "stats_writer.h"
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    stats_writer sw;
+
+    sw >> "keyA" << "value " << 5
+       >> "keyB" << 42
+       >> "keyC" << 101.5;
+
+    std::cout << sw;
+    
+    return 0;
+}
